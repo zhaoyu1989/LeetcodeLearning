bf9d3884b46bf747f9cec855972adeccb0d4da13
zhaoyu1989 <zhaoyu1989@163.com>
2015-01-24 22:44:57 +0800

    quicksort

---
+++ 500QuickSort.java
@@ -0,0 +1,38 @@
+
+
+public class QuickSort {
+	public static void quick_sort(int s[], int l, int r)  
+	{  
+	    if (l < r)  
+	    {  
+	        //Swap(s[l], s[(l + r) / 2]); //有的书将中间值作为参考值。
+	        int i = l, j = r, x = s[l];  
+	        while (i < j)  
+	        {  
+	            while(i < j && s[j] >= x) // 从右向左找第一个小于x的数  
+	                j--;    
+	            if(i < j)   
+	                s[i++] = s[j];  
+	              
+	            while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数  
+	                i++;    
+	            if(i < j)   
+	                s[j--] = s[i];  
+	        }  
+	        s[i] = x;  
+	        quick_sort(s, l, i - 1); // 递归调用   
+	        quick_sort(s, i + 1, r);  
+	    }  
+	} 
+	
+	
+	public static void main(String[] args) {
+		int[] t = {1,4,8,5,2,3};
+		quick_sort(t,0,t.length-1);
+		int i=0;
+		while(i<t.length){
+			System.out.println(t[i]);
+			i++;
+		}
+	}
+}
